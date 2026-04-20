4f08f5330ab2a7c11aabeee070de984f68727b57
Vivi_Fox <vika.pavlenko.161105@gmail.com>
2026-04-20 22:57:29 +0300

    Changed API routes for working with roles and separately with users, added retrieving the list of roles and users, changing a user's role, and deleting accounts.

--- src/app/services/roles.service.ts
+++ src/app/services/roles.service.ts
@@ -2,6 +2,11 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { API_BASE_URL } from '../config/api.config';
+
+/**
+ * RolesService
+ * Handles users and roles admin operations
+ */
 export interface Role {
   id: number;
   name: string;
@@ -13,49 +18,46 @@ export interface User {
   name: string;
   email: string;
   roleId: number;
-  role?: Role;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
-  private apiUrl = `${API_BASE_URL}/roles`;
+
+  private usersUrl = `${API_BASE_URL}/users`;
 
   constructor(private http: HttpClient) { }
 
+  /** Attach JWT token */
   private getAuthHeaders(): HttpHeaders {
-    const token = localStorage.getItem('token'); // or get from AuthService
+    const token = localStorage.getItem('access_token');
     return new HttpHeaders({
       Authorization: `Bearer ${token}`
     });
   }
 
-  /** Get all roles */
-  getRoles(): Observable<Role[]> {
-    return this.http.get<Role[]>(this.apiUrl, {
-      headers: this.getAuthHeaders()
-    });
-  }
-
   /** Get all users */
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.apiUrl}/users/all`, {
+    return this.http.get<User[]>(`${this.usersUrl}/all`, {
       headers: this.getAuthHeaders()
     });
   }
 
   /** Update user role */
   updateUserRole(userId: number, roleId: number): Observable<User> {
-    return this.http.patch<User>(`${this.apiUrl}/users/${userId}/role`, { roleId }, {
-      headers: this.getAuthHeaders()
-    });
+    return this.http.patch<User>(
+      `${this.usersUrl}/${userId}/role`,
+      { roleId },
+      { headers: this.getAuthHeaders() }
+    );
   }
 
   /** Delete user */
   deleteUser(userId: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/users/${userId}`, {
-      headers: this.getAuthHeaders()
-    });
+    return this.http.delete<void>(
+      `${this.usersUrl}/${userId}`,
+      { headers: this.getAuthHeaders() }
+    );
   }
 }

cd2f3c4e42237b32f333953db5b3dbc87794bedc
Vivi_Fox <vika.pavlenko.161105@gmail.com>
2026-04-20 10:02:48 +0300

    Guard has been modified so that it does not allow an authorized user to go to the login page, but redirects them to /user/tasks, and allows an unauthorized user to enter.

--- src/app/guards/auth.guards.ts
+++ src/app/guards/auth.guards.ts
@@ -2,6 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
+/** Guard for user pages */
 export const userPageGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
@@ -9,17 +10,16 @@ export const userPageGuard: CanActivateFn = () => {
   if (authService.isAuthenticated()) {
     return true;
   }
-
   return router.createUrlTree(['/auth']);
 };
 
+/** Guard for auth page */
 export const authPageGuard: CanActivateFn = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (authService.isAuthenticated()) {
-    return router.createUrlTree(['/tasks']);
+    return router.createUrlTree(['/user/tasks']); // correct route
   }
-
   return true;
 };
